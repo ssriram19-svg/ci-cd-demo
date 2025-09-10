788daf99440dc20fa68ef6c861476a0384363cc6
ssriram19-svg <ssriram19@gmail.com>
2025-09-10 15:48:36 +0530

    Update test_extract.py

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,10 @@
 import os
+import sys
 import pandas as pd
+
+# Add parent folder to sys.path so extract.py can be imported
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from extract import extract
 
 def test_extract_creates_file():
@@ -8,5 +13,3 @@ def test_extract_creates_file():
     df = pd.read_csv("extracted.csv")
     assert "id" in df.columns
     assert "name" in df.columns
-
-print("ETL Done!")
